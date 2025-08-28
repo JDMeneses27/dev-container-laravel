ebdcc80e731eab726e53b02bce108f65b777a143
JDMeneses27 <189859220+JDMeneses27@users.noreply.github.com>
2025-08-28 13:52:22 +0000

    feat: :tada: Hicimos el ejercicio de clase

---
+++ laravel-example/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class,'login']);
+
+Route::apiResource ('blog', BlogController::class);
+Route::apiResource ('posts', PostController::class);
